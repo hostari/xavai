5b195b82937a3cbad872d052b88f6b65bef22788
Xavi Ablaza <xavi@hostari.com>
2025-01-27 15:34:08 +0700

    Revert "Use /callback"
    This reverts commit d6d380293e92714a79631801b38a1406df91e4d6.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
+app.post('/line/webhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
